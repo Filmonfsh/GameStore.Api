29207d5ca23c88416fcb065ebe0450a3b2b3264f
Filmonfsh <filmonfsh1234@gmail.com>
2025-03-18 11:57:15 +0200

    Games Add GET, POST, PUT, DELETE

--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82dfc51f8f5f7c21a91399de2ce05e963c3699d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd1dc3d26551907657a8e7c41e8608901594eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
